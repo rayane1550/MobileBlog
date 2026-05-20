27532289295c864c68dde8a0c5bd7a3285bb90e1
Rayane Vitoria Guimarães de Campos <rayrayvguimas26@gmail.com>
2026-05-19 21:45:08 -0300

    Projeto finalizado

--- lib/Pages/pages.interativa.dart
+++ lib/Pages/pages.interativa.dart
@@ -1,6 +1,6 @@
   import 'package:flutter/material.dart';
 
-  class TelaInterativa extends StatefulWidget { // Statefull é para telas que tem interação com o usuário, ou seja, telas que podem mudar de estado. Ele é usado quando a tela precisa atualizar seu conteúdo com base em ações do usuário ou eventos. O StatefulWidget é composto por duas classes: a classe principal (TelaInterativa) e a classe de estado (_TelaInterativaState). A classe de estado é onde a lógica de atualização da tela é implementada, e o método setState() é usado para notificar o framework de que o estado da tela mudou, o que faz com que a tela seja reconstruída com as novas informações.
+  class TelaInterativa extends StatefulWidget {
     const TelaInterativa({super.key});
 
     @override
@@ -33,14 +33,10 @@
               Text(projetos[index]),
               TextButton(onPressed: mudar,child: Text("Mudar")),
               Container(
-                width: MediaQuery.of(context).size.width * 0.4, // o MediaQuery é usado para obter as dimensões da tela do dispositivo e calcular a largura do container como 50% da largura total da tela. Isso torna o layout responsivo, adaptando-se a diferentes tamanhos de tela. RESPONSIVO
+                width: MediaQuery.of(context).size.width * 0.4, 
                 height: MediaQuery.of(context).size.height * 0.4,
                 color: Colors.blue,
               )
-                
-              
-
-              
             ]
 
           )
